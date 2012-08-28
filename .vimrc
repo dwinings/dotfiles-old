761f55035734bae113af633481e8d41a87e43a01
David Winings <wisp558@gmail.com>
2012-08-28 15:51:49 -0400

    Weird vim plugin shenanigans.

--- .vimrc
+++ .vimrc
@@ -9,11 +9,18 @@ set sw=2
 set expandtab
 set foldmethod=manual
 set number
+
+set listchars=tab:>.,trail:.
+set list
+
+
 filetype plugin indent on
 set ofu=syntaxcomplete#Complete
 :highlight Pmenu ctermbg=brown gui=bold
 syntax on
-colorscheme molokai
+let g:solarized_termcolors=256
+set background=dark
+colorscheme solarized
 map <leader>n :NERDTreeToggle<enter>
 
 let g:syntastic_mode_map = { 'mode': 'active',
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,43 +1,51 @@
 import XMonad
+import XMonad.Actions.SpawnOn
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.FadeInactive
 import XMonad.Hooks.SetWMName
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Hooks.FadeInactive
 import System.IO
 
 
 
-myManageHook :: ManageHook
-myManageHook = composeAll 
-    [ className =? "Gimp" --> doFloat
-    , className =? "PyPrompt" --> doFloat
-    , className =? "Evince"   --> doFloat
-    ]
+floatHook :: ManageHook
+floatHook = composeAll 
+  [ className =? "Gimp" --> doFloat
+  , resource =? "pyprompt" --> doFloat
+  , resource =? "evince"   --> doFloat
+  , resource =? "plugin-container" --> doFloat
+  , resource =? "minecraft" --> doFloat
+  ]
 myLogHook :: X ()
 myLogHook = fadeInactiveLogHook fadeAmount
-    where fadeAmount = 0x99999999
+  where fadeAmount = 0.85
+
+workspaceNames = ["1:su", "2:comm", "3:web", "4:vi", "5:dev", "6", "7", "8", "9:bg"]
 
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/wisp/.xmobarrc"
     xmonad $ defaultConfig
-        { manageHook =  manageDocks <+> myManageHook <+> manageHook defaultConfig
-	, terminal = "urxvt"
+      { manageHook =  manageDocks <+> floatHook <+> manageHook defaultConfig
+        , workspaces = workspaceNames
+        , terminal = "urxvt"
         , modMask = mod4Mask
-	, startupHook = setWMName "LG3D"
+        , startupHook = setWMName "LG3D"
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
         , logHook = (dynamicLogWithPP $ xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        }) >> (myLogHook)
-
-        } `additionalKeys`
+            { ppOutput = hPutStrLn xmproc
+            , ppTitle = xmobarColor "green" "" . shorten 50
+            }) >> (myLogHook) 
+      } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
         , ((controlMask, xK_Print), spawn "cd ~/Pictures/screenshots && sleep 0.2; scrot -s ")
         , ((0,       xK_Print), spawn "cd ~/Pictures/screenshots && scrot ")
         , ((mod4Mask, xK_p   ), spawn "exe=`dmenu_run` && eval \"exec $exe\"")
-	, ((mod4Mask, xK_Up  ), spawn "mpc volume +10")
-	, ((mod4Mask, xK_Down), spawn "mpc volume -10")
-	, ((mod4Mask .|. controlMask, xK_m), spawn "mplayer ~/.resources/nyeah.mp3")
+        , ((mod4Mask, xK_Up  ), spawn "mpc volume +10")
+        , ((mod4Mask, xK_Down), spawn "mpc volume -10")
+        , ((mod4Mask,  xK_d),    spawn "~/.scripts/toggle-display.sh")
+        , ((mod4Mask,  xK_e),   spawnOn "8" "xterm")
+        , ((mod4Mask .|. controlMask, xK_m), spawn "mplayer ~/.resources/nyeah.mp3")
         ]
